7f0c3cf7ccc7636a53b24417f42c9305e03a145c
INACAPLudi-Landing-Page <164427281+Dodeline@users.noreply.github.com>
2024-10-21 19:46:28 -0300

    comit

--- evaluaciones/sumativa 1/gestion_productos/productos/urls.py
+++ evaluaciones/sumativa 1/gestion_productos/productos/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import lista_productos
+from .views import lista_productos, detalle_producto, agregar_producto
 
 urlpatterns = [
     path('productos/', lista_productos, name='lista_productos'),
+    path('productos/<int:id>/', detalle_producto, name='detalle_producto'),
+    path('productos/agregar/', agregar_producto, name='agregar_producto'),
 ]
